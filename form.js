39b4f5cefcc12666410fdbc5abdb55068494af2e
sammyy25 <66947139+sammyy25@users.noreply.github.com>
2021-01-04 21:33:35 +0100

    myFirst

---
+++ form.js
@@ -0,0 +1,12 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+
+}
+function openText() {
+  document.getElementById("textarea1").style.display = "block";
+
+}
